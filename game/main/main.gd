17c29d8538ab75aa8d207a8564c97e33d46eb137
pixemisti <66703857+Ayabee527@users.noreply.github.com>
2024-04-04 11:22:09 +0000

    window in animations

--- game/main/main.gd
+++ game/main/main.gd
@@ -11,21 +11,51 @@ func _ready() -> void:
 	screen_size = DisplayServer.screen_get_usable_rect(screen_index)
 	screen_center = screen_size.size / 2
 	
-	center_windows()
+	tween_windows_to_center()
 
-func center_windows() -> void:
-	ready_player_window()
-	ready_jester_window()
+func tween_windows_to_center() -> void:
+	tween_player_window_to_center()
+	jester_window.show()
+	tween_jester_window_to_center()
 
-func ready_player_window() -> void:
+func tween_player_window_to_center() -> void:
+	var window := get_window()
+	window.position = get_player_rest_spot()
+	window.position.y = -window.size.y
+	
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(
+		window, "position", get_player_rest_spot(), 3.0
+	).from_current()
+	tween.play()
+
+func tween_jester_window_to_center() -> void:
+	jester_window.position = get_jester_rest_spot()
+	jester_window.position.y = DisplayServer.screen_get_size(screen_index).y
+	jester_window.position.y += jester_window.size.y
+	jester_window.position.y += jester_window.size.y / 2
+	
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(
+		jester_window, "position", get_jester_rest_spot(), 3.0
+	).from_current()
+	tween.play()
+
+func get_player_rest_spot() -> Vector2i:
 	var window := get_window()
 	
-	window.position = screen_center
-	window.position.y -= window.size.y / 2
-	window.position.x += window.size.x / 4
+	var rest_position = screen_center
+	rest_position.y -= window.size.y / 2
+	rest_position.x += window.size.y / 2
+	
+	return rest_position
 
-func ready_jester_window() -> void:
-	jester_window.position = screen_center
-	jester_window.position.y -= jester_window.size.y / 2
-	jester_window.position.x -= jester_window.size.x
-	jester_window.position.x -= jester_window.size.x / 4
+func get_jester_rest_spot() -> Vector2i:
+	var rest_position: Vector2i = screen_center
+	
+	rest_position = screen_center
+	rest_position.y -= jester_window.size.y / 2
+	rest_position.x -= jester_window.size.x
+	rest_position.x -= jester_window.size.x / 2
+	
+	return rest_position
